cfc5d4c17e2578dc3e21e3f250ab75d1961e36e0
Kate <ktvlphoto@outlook.com>
2023-03-08 21:13:03 +0300

    cycleFor

---
+++ src/lesson5/cycleFor.java
@@ -0,0 +1,11 @@
+package lesson5;
+
+public class cycleFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++){
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+    }
+}
