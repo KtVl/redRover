942cd41a5e4b5d9690c6ead9a2de1966b9b79525
Kate <ktvlphoto@outlook.com>
2023-09-04 14:14:12 +0300

    сложение двух матриц

--- src/codewars/TasksWithArrays.java
+++ src/codewars/TasksWithArrays.java
@@ -8,6 +8,12 @@ public class TasksWithArrays {
         System.out.println(Arrays.toString(digitize(12345)));
         String str = "11 34 3 45 23 5 67";
         System.out.println(Arrays.toString(stringInInt(str)));
+
+        System.out.println("----------");
+
+        int[][] a = new int[][] { { 3, 4, 4 }, { 6, 4, 4 }, { 2, 2, 4 } };
+        int[][] b = new int[][] { { 1, 1, 1 }, { 4, 6, 6 }, { 2, 2, 4 } };
+        System.out.println(Arrays.deepToString(matrixAddition(a,b)));
     }
 
     //Завершите функцию, которая принимает два целых числа ( a, b, где a < b),
@@ -42,5 +48,18 @@ public class TasksWithArrays {
         for (int i = 0; i < strArr.length; i++) {
             numArr[i] = Integer.parseInt(strArr[i]);
         } return numArr;
+    }
+
+    //Напишите функцию, которая принимает две квадратные матрицы (N x N двумерные массивы)
+    // и возвращает их сумму. Обе матрицы, передаваемые в функцию,
+    // будут иметь размер N x N(квадратный) и содержать только целые числа.
+    public static int[][] matrixAddition(int[][] a, int[][] b) {
+        int[][] sum = new int[a.length][a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        } return sum;
     }
 }
